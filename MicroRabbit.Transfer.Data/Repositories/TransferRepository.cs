5fd6a13c7f6e271f83ccb6ae9f1596c23257a691
Georyitt Gorayeb <georyitt_gorayeb@hotmail.com>
2022-07-05 15:02:40 -0400

    consumer rabbitMQ in microservices aspnet6

---
+++ MicroRabbit.Transfer.Data/Repositories/TransferRepository.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+
+namespace MicroRabbit.Transfer.Data.Repositories;
+
+public class TransferRepository : ITransferRepository
+{
+    private TransferDbContext _context;
+
+    public TransferRepository(TransferDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<TransferLog> GetTransferLogs()
+    {
+        return _context.TransferLogs;
+    }
+
+    public void AddTransferLog(TransferLog log)
+    {
+        _context.Add(log);
+        _context.SaveChanges();
+    }
+}
---
+++ RabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace RabbitMQ.Infra.IoC;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //MediatR Mediator
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        
+        // Domain Bus
+        //Domain Bus
+        services.AddSingleton<IEventBus, RabbitMQBus>(sp => { 
+            var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+            var optionsFactory = sp.GetService<IOptions<RabbitMQSettings>>();
+            return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory, optionsFactory );
+        });
+        
+        return services;
+    }
+}
